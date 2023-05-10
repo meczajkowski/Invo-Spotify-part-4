fcc9dae90f65f499759ff3fcb320e58185e650a7
Michal <michal.erik.czajkowski@gmail.com>
2023-05-10 08:32:50 +0200

    change: making the accordion smooth

--- src/pages/home-page/components/faq-component/faq-component.js
+++ src/pages/home-page/components/faq-component/faq-component.js
@@ -13,5 +13,12 @@ export const FaqComponent = () => {
       // Toggle the 'data-question-expanded' attribute when the question is clicked
       faqSingleQuestion.toggleAttribute("data-question-expanded");
     });
+    // Get the answer element for the current question
+    const faqAnswer = faqSingleQuestion.querySelector('.faq-question-partial__answer');
+
+    // Add the CSS custom property '--faq-answer-height' for the answer element
+    // Set its value to the scrollHeight property of the answer element,
+    // which shows the full height of the element even when its current height is 0
+    faqSingleQuestion.style.setProperty('--faq-answer-height', `${faqAnswer.scrollHeight}px`);
   });
 };
