e395381d8044bb4e93cdaceec4e6c345bc77b98b
Michal <michal.erik.czajkowski@gmail.com>
2023-05-10 09:00:29 +0200

    change: Listening to answer size changes and updating values of its height

--- src/pages/home-page/components/faq-component/faq-component.js
+++ src/pages/home-page/components/faq-component/faq-component.js
@@ -7,18 +7,23 @@ export const FaqComponent = () => {
     ".faq-question-partial"
   );
 
+  function listenToQuestionResize(faqQuestionToListen) {
+    const faqQuestion = faqQuestionToListen[0].target;
+
+    const faqAnswer = faqQuestion.querySelector(
+      ".faq-question-partial__answer"
+    );
+
+    faqQuestion.style.setProperty(
+      "--faq-answer-height",
+      `${faqAnswer.scrollHeight}px`
+    );
+  }
+
   faqAllQuestions.forEach((faqSingleQuestion) => {
-    // Add a click event listener to each question
+    new ResizeObserver(listenToQuestionResize).observe(faqSingleQuestion);
     faqSingleQuestion.addEventListener("click", () => {
-      // Toggle the 'data-question-expanded' attribute when the question is clicked
       faqSingleQuestion.toggleAttribute("data-question-expanded");
     });
-    // Get the answer element for the current question
-    const faqAnswer = faqSingleQuestion.querySelector('.faq-question-partial__answer');
-
-    // Add the CSS custom property '--faq-answer-height' for the answer element
-    // Set its value to the scrollHeight property of the answer element,
-    // which shows the full height of the element even when its current height is 0
-    faqSingleQuestion.style.setProperty('--faq-answer-height', `${faqAnswer.scrollHeight}px`);
   });
 };
